c92b3e44d1a3f4a2a78f1772a940a8009a309f68
Clement Yuen <ccyuen@edu.uwaterloo.ca>
2017-09-17 13:21:07 -0400

    added branch google-maps to implement google maps for request pins UI

--- HelpAFriend/app/src/main/java/com/example/staff/helpafriend/MainActivity.java
+++ HelpAFriend/app/src/main/java/com/example/staff/helpafriend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.staff.helpafriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -32,42 +33,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(this, googlemaps.class);
+        startActivity(intent);
         // Views
-        mStatusTextView = (TextView) findViewById(R.id.status);
-        mDetailTextView = (TextView) findViewById(R.id.detail);
-        mEmailField = (EditText) findViewById(R.id.field_email);
-        mPasswordField = (EditText) findViewById(R.id.field_password);
+//        mStatusTextView = (TextView) findViewById(R.id.status);
+//        mDetailTextView = (TextView) findViewById(R.id.detail);
+//        mEmailField = (EditText) findViewById(R.id.field_email);
+//        mPasswordField = (EditText) findViewById(R.id.field_password);
 
         // Buttons
-        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
-        findViewById(R.id.email_create_account_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
-        findViewById(R.id.verify_email_button).setOnClickListener(this);
+//        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
+//        findViewById(R.id.email_create_account_button).setOnClickListener(this);
+//        findViewById(R.id.sign_out_button).setOnClickListener(this);
+//        findViewById(R.id.verify_email_button).setOnClickListener(this);
 
         // [START initialize_auth]
-        mAuth = FirebaseAuth.getInstance();
-        auth = new Authentication(this, mAuth);
+        //mAuth = FirebaseAuth.getInstance();
+        //auth = new Authentication(this, mAuth);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        auth.updateUI(currentUser);
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        auth.updateUI(currentUser);
+//    }
 
-    @Override
-    public void onClick(View v) {
-        int i = v.getId();
-        if (i == R.id.email_create_account_button) {
-            auth.createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        } else if (i == R.id.email_sign_in_button) {
-            auth.signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        } else if (i == R.id.sign_out_button) {
-            auth.signOut();
-        } else if (i == R.id.verify_email_button) {
-            auth.sendEmailVerification();
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        int i = v.getId();
+//        if (i == R.id.email_create_account_button) {
+//            auth.createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
+//        } else if (i == R.id.email_sign_in_button) {
+//            auth.signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+//        } else if (i == R.id.sign_out_button) {
+//            auth.signOut();
+//        } else if (i == R.id.verify_email_button) {
+//            auth.sendEmailVerification();
+//        }
+//    }
 }
